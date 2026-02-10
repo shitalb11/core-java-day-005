4c8f69502cd97a97c19ce06f4fe5d95bd3bc600a
Shital Bhosale <shitalb075@gmail.com>
2026-02-10 13:05:48 +0530

    demo2

---
+++ src/main/java/org/example/StringBasedPrograms/Demo2.java
@@ -0,0 +1,11 @@
+package org.example.StringBasedPrograms;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        int[] a={1,2,3};
+        int sum = 0;
+        for (int i:a) sum+=i;
+        System.out.println(sum);
+    }
+}
+//sum of array
