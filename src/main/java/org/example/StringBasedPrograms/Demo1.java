97892b6b69e13c6a59845894eacec1cb627e300a
Shital Bhosale <shitalb075@gmail.com>
2026-02-10 12:40:00 +0530

    demo1

---
+++ src/main/java/org/example/StringBasedPrograms/Demo1.java
@@ -0,0 +1,8 @@
+package org.example.StringBasedPrograms;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        String string ="Hello java";
+        System.out.println(string);
+    }
+}
