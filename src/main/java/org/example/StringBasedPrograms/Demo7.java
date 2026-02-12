4a31b3b72428ad5c77f67a03c23d193c58b6de99
Shital Bhosale <shitalb075@gmail.com>
2026-02-12 14:48:04 +0530

    Demo7

---
+++ src/main/java/org/example/StringBasedPrograms/Demo7.java
@@ -0,0 +1,10 @@
+package org.example.StringBasedPrograms;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        String string = "Java2121";
+        System.out.println(string.length());
+    }
+}
+
+//Count Characters and numbers also
