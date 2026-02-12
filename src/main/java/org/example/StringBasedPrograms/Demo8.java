840046cd4f429f02682299af923b87b2eda72cac
Shital Bhosale <shitalb075@gmail.com>
2026-02-12 14:52:52 +0530

    demo 8 and 9

---
+++ src/main/java/org/example/StringBasedPrograms/Demo8.java
@@ -0,0 +1,10 @@
+package org.example.StringBasedPrograms;
+
+public class Demo8 {
+    public static void main(String[] args) {
+       String string = "Java Is Fun";
+        System.out.println(string.replace(" ",""));
+    }
+}
+
+//Remove Spaces from String
---
+++ src/main/java/org/example/StringBasedPrograms/Demo9.java
@@ -0,0 +1,11 @@
+package org.example.StringBasedPrograms;
+
+public class Demo9 {
+    public static void main(String[] args) {
+       String string = "Java";
+        System.out.println(string.toUpperCase());
+        System.out.println(string.toLowerCase());
+    }
+}
+
+//Uppercase / Lowercase
