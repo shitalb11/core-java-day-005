579da55ced14e6f6e8ccd944b1d9059c87b8a463
Shital Bhosale <shitalb075@gmail.com>
2026-02-13 20:55:59 +0530

    demo10

---
+++ src/main/java/org/example/StringBasedPrograms/Demo10.java
@@ -0,0 +1,11 @@
+package org.example.StringBasedPrograms;
+
+public class Demo10 {
+    public static void main(String[] args) {
+        String string = "Java is very easy";
+        String[] words= string.split(" ");
+        System.out.println(words.length);
+    }
+}
+
+//Count Words in Sentence
