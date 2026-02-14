55a6c5762acd47d6c3801cd2a6ecbeeaadfe0f59
Shital Bhosale <shitalb075@gmail.com>
2026-02-14 23:05:28 +0530

    demo13

---
+++ src/main/java/org/example/StringBasedPrograms/Demo13.java
@@ -0,0 +1,10 @@
+package org.example.StringBasedPrograms;
+
+public class Demo13 {
+    public static void main(String[] args) {
+        String string = "java";
+        System.out.println(string.replace('a', 'z'));
+    }
+}
+
+//Replace Character
